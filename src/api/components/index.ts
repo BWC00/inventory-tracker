1da889ad34f635a26cff296005d3ce24c48d8b21
mohammedshakleya <shaki2632@gmail.com>
2023-07-12 07:45:30 +0200

    feat: Registers routes and middlewares.

---
+++ src/api/components/index.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+
+/**
+ * Init component routes
+ *
+ * @param {Router} router
+ * @param {string} prefix
+ * @returns {void}
+ */
+export function registerApiRoutes(router: Router, prefix: string = ''): void {
+
+}
--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,5 +1,8 @@
 import { Request, Response, Router } from 'express';
 
+import { registerApiRoutes } from './components';
+import { registerErrorHandler, registerMiddleware } from './middleware';
+
 /**
  * Init Express REST routes
  *
@@ -10,4 +13,8 @@ export function initRestRoutes(router: Router): void {
 	const prefix: string = '/api/v1';
 
 	router.get(prefix, (req: Request, res: Response) => res.send('PING'));
+
+	registerMiddleware(router);
+	registerApiRoutes(router, prefix);
+	registerErrorHandler(router);
 }
