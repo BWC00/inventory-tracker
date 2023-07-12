56bae9a43a7e51fcdb64efd8b2d6f70ddde699c8
mohammedshakleya <shaki2632@gmail.com>
2023-07-12 20:37:41 +0200

    feat: Support for emails with multiple receivers.

--- src/services/mail.ts
+++ src/services/mail.ts
@@ -1,6 +1,8 @@
 import { createTransport, SendMailOptions, SentMessageInfo, Transporter, TransportOptions } from 'nodemailer';
 
 import { env, mails } from '../config/globals';
+// import { logger } from '../config/logger';
+
 
 
 /**
@@ -18,13 +20,17 @@ export class MailService {
 	 * @param forceSend Force email to be sent
 	 * @returns info of sent mail
 	 */
-	public sendMail(email: string, subject: string, text: string): Promise<SentMessageInfo> | void {
+	public sendMail(emails: string[], subject: string, text: string): Promise<SentMessageInfo> | void {
+		// if (env.NODE_ENV === 'production' || forceSend) {
+		// 	return this.transporter.sendMail(options);
+		// }
+		// logger.info('Emails are only sent in production mode!');
 
 		const options: SendMailOptions = {
 			from: mails.sender,
 			subject: subject,
 			text: text,
-			to: email
+			to: emails
 		};
 
 		return this.transporter.sendMail(options);
